bf03dfba3a2062c0d66acfd666aa4245e09bbebf
yaseenali512 <yaseenali.bscssef20@iba-suk.edu.pk>
2023-10-24 15:47:15 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,119 @@
+import { useState } from "react";
+const content = [
+  {
+    summary: "React is a library for building UIs",
+    details:
+      "Dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  },
+  {
+    summary: "State management is like giving state a home",
+    details:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  },
+  {
+    summary: "We can think of props as the component API",
+    details:
+      "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+  },
+];
+
+export default function App() {
+  return (
+    <div>
+      <Tabbed content={content} />
+    </div>
+  );
+}
+
+function Tabbed({ content }) {
+  const [activeTab, setActiveTab] = useState(0);
+
+  return (
+    <>
+      <div>
+        <div className="tabs">
+          <Tab num={0} activeTab={activeTab} onClick={setActiveTab} />
+          <Tab num={1} activeTab={activeTab} onClick={setActiveTab} />
+          <Tab num={2} activeTab={activeTab} onClick={setActiveTab} />
+          <Tab num={3} activeTab={activeTab} onClick={setActiveTab} />
+        </div>
+
+        {activeTab <= 2 ? (
+          <TabContent
+            item={content.at(activeTab)}
+            key={content.at(activeTab).summary}
+          />
+        ) : (
+          <DifferentContent />
+        )}
+      </div>
+    </>
+  );
+}
+
+function Tab({ num, activeTab, onClick }) {
+  return (
+    <button
+      className={activeTab === num ? "tab active" : "tab"}
+      onClick={() => onClick(num)}
+    >
+      Tab {num + 1}
+    </button>
+  );
+}
+
+function TabContent({ item }) {
+  const [showDetails, setShowDetails] = useState(true);
+  const [likes, setLikes] = useState(0);
+
+  function handleInc() {
+    setLikes(likes + 1);
+    //render batch
+  }
+
+  function handleUndo() {
+    setShowDetails(true);
+    setLikes(0);
+  }
+
+  function trippleInc() {
+    setLikes((likes) => likes + 3);
+  }
+
+  function handleUndoLater() {
+    setTimeout(handleUndo, 2000);
+    //render batch
+  }
+
+  return (
+    <div className="tab-content">
+      <h4>{item.summary}</h4>
+      {showDetails && <p>{item.details}</p>}
+
+      <div className="tab-actions">
+        <button onClick={() => setShowDetails((h) => !h)}>
+          {showDetails ? "Hide" : "Show"} details
+        </button>
+
+        <div className="hearts-counter">
+          <span>{likes} ❤️</span>
+          <button onClick={handleInc}>+</button>
+          <button onClick={trippleInc}>+++</button>
+        </div>
+      </div>
+
+      <div className="tab-undo">
+        <button onClick={handleUndo}>Undo</button>
+        <button onClick={handleUndoLater}>Undo in 2s</button>
+      </div>
+    </div>
+  );
+}
+
+function DifferentContent() {
+  return (
+    <div className="tab-content">
+      <p>Different Content</p>
+    </div>
+  );
+}
